f90a2a8c6cb7c68b19ceef4727e571373c62cf73
fkvesi00 <fkvesi00@fesb.hr>
2024-01-09 18:29:02 +0100

    chaging routes

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
   useEffect(()=>{
     const fetchData = async () => {
      
-      const raspored = await fetch("http://52.59.252.228:5001/api/matches/allMatches",{
+      const raspored = await fetch("http://52.59.252.228:5001/matches/allMatches",{
         method:'POST',
         headers:{'Content-Type':'application/json'},
         body:JSON.stringify({
@@ -40,7 +40,7 @@ function App() {
         })
       })
       
-      const team = await fetch("http://52.59.252.228:5001/api/clubs/season",{
+      const team = await fetch("http://52.59.252.228:5001/clubs/season",{
         method:'POST',
         headers:{'Content-Type':'application/json'},
         body:JSON.stringify({
