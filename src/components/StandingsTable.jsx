8e37484f1de025ef43f40cfa670e57d980e7a740
fkvesi00 <fkvesi00@fesb.hr>
2024-01-12 17:13:50 +0100

    making it look little bit better

--- src/components/StandingsTable.jsx
+++ src/components/StandingsTable.jsx
@@ -21,7 +21,7 @@ const StandingsTable = ({ tablica }) => {
   return (
     <div className="overflow-x-auto my-10">
       <div className="header text-center text-sm mb-2">TABLICA</div>
-      <table className="table-auto w-full" data-theme="night">
+      <table className="table-auto w-full sm:w-auto md:w-auto lg:w-auto xl:w-auto mx-auto" data-theme="night">
         <thead>
           <tr>
             <th className="p-2 text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">Rank</th>
@@ -36,7 +36,7 @@ const StandingsTable = ({ tablica }) => {
             <th className="p-2 text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">Pts</th>
           </tr>
         </thead>
-        <tbody>{rows}</tbody>
+        <tbody className="divide-y divide-gray-300">{rows}</tbody>
       </table>
     </div>
   );
