b4c970f8a6d9743b288ac7792616d68208909680
fkvesi00 <fkvesi00@fesb.hr>
2024-01-12 14:54:37 +0100

    delete import of table

--- src/components/Klub.jsx
+++ src/components/Klub.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import Table from './Table';
 import Raspored from './Raspored';
 import Utakmice from './Utakmice';
 
