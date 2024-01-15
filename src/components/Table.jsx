0ad5d5a6c14c599238834114411d4f26e2a2573f
fkvesi00 <fkvesi00@fesb.hr>
2024-01-15 10:33:16 +0100

    css change

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,9 +4,8 @@ import {Link} from 'react-router-dom';
 function Table({rank,name,played,w,d,l,gf,ga,points,id}) {
   return (
     <tr data-theme='fantasy'>
-    <Link to={`klub/${id}`}>
     <td className='bg-neutral text-white w-8 h-8 rounded-full flex items-center justify-center'>{rank}</td>
-    <td >{name}</td>
+    <td ><Link to={`klub/${id}`}>{name}</Link></td>
     <td className='text-center'>{played}</td>
     <td className='text-center'>{w}</td>
     <td className='text-center'>{d}</td>
@@ -15,7 +14,7 @@ function Table({rank,name,played,w,d,l,gf,ga,points,id}) {
     <td className='text-center'>{ga}</td>
     <td className='text-center'>{gf - ga}</td>
     <td data-theme='retro' className='text-center'>{points}</td>
-    </Link>
+    
   </tr>
   )
 }
