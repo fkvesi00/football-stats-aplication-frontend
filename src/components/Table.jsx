53bab5c0ebeb33fcd8e0cd0c4109d35751726956
fkvesi00 <fkvesi00@fesb.hr>
2024-01-15 10:31:29 +0100

    adding css to table component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,9 +3,9 @@ import {Link} from 'react-router-dom';
 
 function Table({rank,name,played,w,d,l,gf,ga,points,id}) {
   return (
-    <tr data-theme='fantasy' className='mb-3'>
-      <Link to={`klub/${id}`}>
-    <td className='text-center'>{rank}</td>
+    <tr data-theme='fantasy'>
+    <Link to={`klub/${id}`}>
+    <td className='bg-neutral text-white w-8 h-8 rounded-full flex items-center justify-center'>{rank}</td>
     <td >{name}</td>
     <td className='text-center'>{played}</td>
     <td className='text-center'>{w}</td>
