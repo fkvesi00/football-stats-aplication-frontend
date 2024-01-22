f031faae7dea455b6abadbd0ea739b556b805342
fkvesi00 <fkvesi00@fesb.hr>
2024-01-22 16:21:13 +0100

    center text

--- src/components/Rasporedcic.jsx
+++ src/components/Rasporedcic.jsx
@@ -4,7 +4,7 @@ function Rasporedcic({time,date,home,away,id}) {
   return (
     <tr className="align-middle" data-theme="fantasy">
       <td className="p-1 text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl" style={{ fontFamily: 'Arial, Helvetica, sans-serif' }}>{date}</td>
-      <td>{time}</td>
+      <td className="text-center">{time}</td>
       <td className="text-center">
         <div className="p-1" style={{ fontFamily: 'Arial, Helvetica, sans-serif' }}>{home}</div>
         <div className="p-1" style={{ fontFamily: 'Arial, Helvetica, sans-serif' }}>{away}</div>
