ab2a50f36f9386801c82d8ecb94964ec197b685d
fkvesi00 <fkvesi00@fesb.hr>
2024-01-22 18:57:21 +0100

    making logo rounded and align it with name

--- src/components/Utakmica.jsx
+++ src/components/Utakmica.jsx
@@ -4,20 +4,20 @@ import { faPlayCircle } from '@fortawesome/free-regular-svg-icons';
 import { Link } from 'react-router-dom';
 
 function Utakmica({ MatchID, Date, HomeTeamID, HomeTeamScore, AwayTeamName, a_id, h_id }) {
-  const h_logo = `/images/${h_id}.jpg`
-  const a_logo = `/images/${a_id}.jpg`
-  
+  const h_logo = `/images/${h_id}.jpg`;
+  const a_logo = `/images/${a_id}.jpg`;
+
   return (
     <tr className="align-middle" data-theme="fantasy">
       <td className="p-1 text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl" style={{ fontFamily: 'Arial, Helvetica, sans-serif' }}>{Date}</td>
       <td className="text-center">
-        <div className="p-1" style={{ fontFamily: 'Arial, Helvetica, sans-serif' }}>
-          <img src={h_logo} alt={`Logo for ${HomeTeamID}`} width="20" height="20" className="mr-2" />
-          {HomeTeamID}
+        <div className="flex items-center p-1" style={{ fontFamily: 'Arial, Helvetica, sans-serif' }}>
+          <img src={h_logo} alt={`Logo for ${HomeTeamID}`} width="30" height="30" className="mr-2 rounded-full" />
+          <span>{HomeTeamID}</span>
         </div>
-        <div className="p-1" style={{ fontFamily: 'Arial, Helvetica, sans-serif' }}>
-          <img src={a_logo} alt={`Logo for ${a_id}`} width="20" height="20" className="mr-2" />
-          {AwayTeamName}
+        <div className="flex items-center p-1" style={{ fontFamily: 'Arial, Helvetica, sans-serif' }}>
+          <img src={a_logo} alt={`Logo for ${a_id}`} width="30" height="30" className="mr-2 rounded-full" />
+          <span>{AwayTeamName}</span>
         </div>
       </td>
       <td className="text-center p-1 text-sm sm:text-m md:text-base lg:text-lg xl:text-lg 2xl:text-xl" style={{ backgroundColor: '#130F2A', fontWeight: 'bold', color: 'white' }}>
@@ -30,7 +30,7 @@ function Utakmica({ MatchID, Date, HomeTeamID, HomeTeamScore, AwayTeamName, a_id
         </Link>
       </td>
     </tr>
-  )
+  );
 }
 
 export default Utakmica;
