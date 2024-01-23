48cdc00f66c49effae371cd7d8694b4290cd7fc3
fkvesi00 <fkvesi00@fesb.hr>
2024-01-23 15:45:37 +0100

    add logo of leauge

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,38 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function NavBar({klubovi}) {
+function NavBar({ klubovi }) {
   return (
-    <div className="navbar bg-base-100 mb" data-theme="retro" >
-        <div className="flex-1">
+    <div className="navbar bg-base-100 mb" data-theme="retro">
+      <div className="flex-1">
         <Link to='/' className="btn btn-ghost normal-case text-xl">
-            UMA Metković
+          UMA Metković
         </Link>
-        </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-      <li tabIndex={0}>
-        <Link to='#'>
-          <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-        </Link>
-        <ul className="p-2 bg-base-100">
-          <li><Link to='/klubovi'>Klubovi</Link></li>
-          <li><Link to='/igraci'>Igrači</Link></li>
+      </div>
+
+      <div className="flex-1 text-center">
+        <img src="/images/0.jpg" alt="Logo" className="w-12 h-12 rounded-full" />
+      </div>
+
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li tabIndex={0}>
+            <Link to='#'>
+              <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/>
+              </svg>
+            </Link>
+            <ul className="p-2 bg-base-100">
+              <li><Link to='/klubovi'>Klubovi</Link></li>
+              <li><Link to='/igraci'>Igrači</Link></li>
+            </ul>
+          </li>
+          <li><Link to='/ '>Liga</Link></li>
+          <li><Link to='/signIn'>Admin</Link></li>
         </ul>
-      </li>
-      <li><Link to='/ '>Liga</Link></li>
-      <li><Link to='/signIn'>Admin</Link></li>
-    </ul>
-  </div>
-</div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
