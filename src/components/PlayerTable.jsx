f3b5895920defb5e39da000d9f64882976c24eb3
fkvesi00 <fkvesi00@fesb.hr>
2024-01-23 20:56:06 +0100

    adding fa icon

--- src/components/PlayerTable.jsx
+++ src/components/PlayerTable.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFutbol } from '@fortawesome/free-solid-svg-icons';
 
-function PlayerTable({igrac, gol}) {
-    return (
-        <tr data-theme='fantasy' style={{padding:'10px'}} >
-          <td style={{borderRight: "1px solid black", textAlign:'center'}}>{igrac}</td>
-          <td style={{borderRight: "1px solid black", textAlign:'center'}}>{gol}</td>
-        </tr>
-      )
+function PlayerTable({ igrac, gol }) {
+  // Create an array with the length equal to the number of goals
+  const goalsArray = Array.from({ length: gol }, (_, index) => index);
+
+  return (
+    <tr data-theme='fantasy' style={{ padding: '10px', textAlign: 'center' }}>
+      <td style={{ borderRight: '1px solid black', textAlign: 'center' }}>{igrac}</td>
+      <td style={{ borderRight: '1px solid black', textAlign: 'center' }}>
+        {/* Map over the goalsArray to render the football icons */}
+        {goalsArray.map((goalIndex) => (
+          <FontAwesomeIcon key={goalIndex} icon={faFutbol} style={{ margin: '0 2px' }} />
+        ))}
+      </td>
+    </tr>
+  );
 }
 
-export default PlayerTable
+export default PlayerTable;
