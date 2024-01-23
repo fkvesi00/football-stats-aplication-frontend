07d7b81188a367c86a49dc0498556c67f0088209
fkvesi00 <fkvesi00@fesb.hr>
2024-01-23 19:43:47 +0100

    adding css

--- src/components/Rezultat.jsx
+++ src/components/Rezultat.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 
 const Rezultat = ({ matchID, date, time, homeTeamName, score, awayTeamName, a_id, h_id }) => {
-    const h_logo = `/images/${h_id}.jpg`
-    const a_logo = `/images/${a_id}.jpg`
+    const h_logo = `/images/${h_id}.jpg`;
+    const a_logo = `/images/${a_id}.jpg`;
 
     return (
         <div className="flex flex-col sm:flex-row md:flex-row lg:flex-row xl:flex-row items-center justify-center">
             {/* Home Team Section */}
-            <div className="sm:flex-1 md:flex-1 lg:flex-1 xl:flex-1 text-center sm:mr-0 md:mr-16 lg:mr-16 xl:mr-16 mb-4 sm:mb-0 md:mb-0 lg:mb-0 xl:mb-0">
-                {/* Home Team Logo (Large & Rounded, Responsive) */}
-                <img src={h_logo} alt={homeTeamName} className="w-full rounded-full mx-auto mb-2" />
+            <div className="flex-1 text-center mb-4 sm:mb-0">
+                {/* Home Team Logo (Responsive) */}
+                <img src={h_logo} alt={homeTeamName} className="w-full sm:w-16 md:w-24 lg:w-32 xl:w-40 rounded-full mx-auto mb-2" />
 
                 {/* Home Team Name (Medium & Centered) */}
                 <p className="text-base text-center">{homeTeamName}</p>
             </div>
 
             {/* Match Information Section (Small) */}
-            <div className="sm:flex-1 md:flex-1 lg:flex-1 xl:flex-1 text-center sm:mr-0 md:mr-16 lg:mr-16 xl:mr-16 mb-4 sm:mb-0 md:mb-0 lg:mb-0 xl:mb-0">
+            <div className="flex-1 text-center mb-4 sm:mb-0">
                 {/* Date (Small) */}
                 <p className="text-xs">{date}</p>
 
@@ -28,9 +28,9 @@ const Rezultat = ({ matchID, date, time, homeTeamName, score, awayTeamName, a_id
             </div>
 
             {/* Away Team Section */}
-            <div className="sm:flex-1 md:flex-1 lg:flex-1 xl:flex-1 text-center sm:mb-0 md:mb-0 lg:mb-0 xl:mb-0">
-                {/* Away Team Logo (Large & Rounded, Responsive) */}
-                <img src={a_logo} alt={awayTeamName} className="w-full rounded-full mx-auto mb-2" />
+            <div className="flex-1 text-center">
+                {/* Away Team Logo (Responsive) */}
+                <img src={a_logo} alt={awayTeamName} className="w-full sm:w-16 md:w-24 lg:w-32 xl:w-40 rounded-full mx-auto mb-2" />
 
                 {/* Away Team Name (Medium & Centered) */}
                 <p className="text-base text-center">{awayTeamName}</p>
