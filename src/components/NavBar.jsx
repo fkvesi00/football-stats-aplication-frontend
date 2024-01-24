de33ade29e3ae4a8c66856eed2ceabbb79cef01c
fkvesi00 <fkvesi00@fesb.hr>
2024-01-24 23:54:50 +0100

    chaging style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,33 +3,31 @@ import { Link } from 'react-router-dom';
 
 function NavBar({ klubovi }) {
   const textStyle = {
-    color: 'white', // Set text color to white
-    fontFamily: 'Lucida Console, Monaco, monospace', // Set font family
+    color: 'white',
+    fontFamily: 'Lucida Console, Monaco, monospace',
+  };
+
+  const rowStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   };
 
   return (
     <div className="navbar bg-base-100 mb" data-theme="night">
-      <div className="flex-1">
-        <Link to='/' className="btn btn-ghost normal-case text-xl" >
+      {/* First Row */}
+      <div style={{ ...rowStyle, marginBottom: '8px' }}>
+        <Link to='/' className="btn btn-ghost normal-case text-xl" style={{color:'white'}}>
           UMA Metković
         </Link>
       </div>
 
-      <div className="flex-none">
+      {/* Second Row */}
+      <div style={rowStyle}>
         <ul className="menu menu-horizontal px-1">
-          <li tabIndex={0}>
-            <Link to='#' style={textStyle}>
-              <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/>
-              </svg>
-            </Link>
-            <ul className="p-2 bg-base-100">
-              <li><Link to='/klubovi' style={textStyle}>Klubovi</Link></li>
-              <li><Link to='/igraci' style={textStyle}>Igrači</Link></li>
-            </ul>
-          </li>
-          <li><Link to='/ ' style={textStyle}>Liga</Link></li>
-          <li><Link to='/signIn' style={textStyle}>Sign in</Link></li>
+          <li><Link to='/klubovi' style={textStyle}>Klubovi</Link></li>
+          <li><Link to='/igraci' style={textStyle}>Igrači</Link></li>
+          <li><Link to='/signIn' style={textStyle}>Sign In</Link></li>
         </ul>
       </div>
     </div>
