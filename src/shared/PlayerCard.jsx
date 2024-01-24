b3eb9a28d9105aa65159ca6435356c3c9be1fac6
fkvesi00 <fkvesi00@fesb.hr>
2024-01-25 00:18:00 +0100

    adding dummy picture for players

--- src/shared/PlayerCard.jsx
+++ src/shared/PlayerCard.jsx
@@ -5,7 +5,7 @@ function PlayerCard({ime,godine,nacionalnost,slika,id}) {
     <Link to={`/igrac/${id}`}>
       
     <div className="card w-64 bg-base-100 shadow-xl p-4 m-2 " >
-    <figure style={{height:"100px"}}><img src="https://www.kineosystem.com/wp-content/uploads/2016/08/dummy-prod-1.jpg" alt="" /></figure>
+    <figure style={{height:"100px"}}><img src="/images/dummy-profile.jpg" alt="" /></figure>
     <div className="card-body">
       <h2 className="card-title">{ime}</h2>
       <p>Godina: {godine}</p>
