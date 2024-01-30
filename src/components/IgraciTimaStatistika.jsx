45cf5cf56bf0564267ac084317c738ac594c9dbe
fkvesi00 <fkvesi00@fesb.hr>
2024-01-30 20:04:59 +0100

    adding new background color

--- src/components/IgraciTimaStatistika.jsx
+++ src/components/IgraciTimaStatistika.jsx
@@ -4,7 +4,7 @@ function IgraciTimaStatistika({counter,playerid, playerName, app, goals}) {
   return (
     <tr data-theme='fantasy' style={{padding:'10px'}} >
         <td style={{borderRight: "1px solid black", textAlign:'center', fontFamily: 'Lucida Console, Monaco, monospace'}}>{counter+1}</td>
-        <td style={{borderRight: "1px solid black", textAlign: 'center', fontFamily: 'Lucida Console, Monaco, monospace', backgroundColor: 'OliveDrab', color: 'white'}}>{playerName}</td>
+        <td style={{borderRight: "1px solid black", textAlign: 'center', fontFamily: 'Lucida Console, Monaco, monospace', backgroundColor: '#166546', color: 'white'}}>{playerName}</td>
         <td style={{borderRight: "1px solid black", textAlign:'center', fontFamily: 'Lucida Console, Monaco, monospace'}}>{goals}</td>
         <td style={{borderRight: "1px solid black", textAlign:'center', fontFamily: 'Lucida Console, Monaco, monospace'}}>{app}</td>
     </tr>
