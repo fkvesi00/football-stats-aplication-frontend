b5f762fe92ce9d03f299b8991095bd60c0f8ab9c
fkvesi00 <fkvesi00@fesb.hr>
2024-02-02 15:05:48 +0100

    adding ghost to buttns

--- src/components/NavBarClub.jsx
+++ src/components/NavBarClub.jsx
@@ -17,12 +17,12 @@ function NavBarClub({handleClick}) {
   return (
     <div className="bg-base-100 mb" data-theme="garden">
      <div style={rowStyle}>
-        <ul className="menu menu-horizontal px-1 btn btn-ghost" style={{textAlign:'center', justifyContent:'center'}}>
-          <li><div style={textStyle} onClick={()=> handleClick('Raspored')}>Raspored</div></li>
-          <li><div style={textStyle} onClick={()=> handleClick('Utakmice')}>Utakmice</div></li>
-          <li><div style={textStyle} onClick={()=> handleClick('Igrači')}>Igrači</div></li>
-          <li><div style={textStyle} onClick={()=> handleClick('Statistika')}>Statistika</div></li>
-          <li><div style={textStyle} onClick={()=> handleClick('Galerija')}>Galerija</div></li>
+        <ul className="menu menu-horizontal px-1" style={{textAlign:'center', justifyContent:'center'}}>
+          <li><div className="btn btn-ghost" style={textStyle} onClick={()=> handleClick('Raspored')}>Raspored</div></li>
+          <li><div className="btn btn-ghost" style={textStyle} onClick={()=> handleClick('Utakmice')}>Utakmice</div></li>
+          <li><div className="btn btn-ghost" style={textStyle} onClick={()=> handleClick('Igrači')}>Igrači</div></li>
+          <li><div className="btn btn-ghost" style={textStyle} onClick={()=> handleClick('Statistika')}>Statistika</div></li>
+          <li><div className="btn btn-ghost" style={textStyle} onClick={()=> handleClick('Galerija')}>Galerija</div></li>
         </ul>
       </div>
     </div>
