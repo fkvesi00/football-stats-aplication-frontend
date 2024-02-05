dc1d84d098fb1317d3cb3aa0ebfa83d33e360039
fkvesi00 <fkvesi00@fesb.hr>
2024-02-05 14:42:31 +0100

    fixing import

--- src/components/Strijelci.jsx
+++ src/components/Strijelci.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import StrijelciTable from './StrijelciTable';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faFutbol } from '@fortawesome/free-solid-svg-icons';
 
