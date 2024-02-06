760f07de107268431c4c495d18cd613b69398545
fkvesi00 <fkvesi00@fesb.hr>
2024-02-06 20:45:40 +0100

    adding new style to ljestvica strijleca

--- src/components/StrijelciTable.jsx
+++ src/components/StrijelciTable.jsx
@@ -6,7 +6,7 @@ function StrijelciTable({counter,playerid, playerName, app, goals,teamName}) {
         <td style={{borderRight: "1px solid black", textAlign:'center', fontFamily: 'Lucida Console, Monaco, monospace'}}>{counter+1}</td>
         <td style={{borderRight: "1px solid black", textAlign: 'left', fontFamily: 'Lucida Console, Monaco, monospace', backgroundColor: '#0b5437', color: 'white'}}>{playerName}</td>
         <td style={{borderRight: "1px solid black", textAlign:'left', fontFamily: 'Montserrat'}}>{teamName}</td>
-        <td style={{borderRight: "1px solid black", textAlign:'center', fontFamily: 'Montserrat'}}>{goals}</td>
+        <td style={{borderRight: "1px solid black", textAlign:'center', fontFamily: 'Montserrat', fontWeight: 'bold'}} data-theme="garden">{goals}</td>
         <td style={{borderRight: "1px solid black", textAlign:'center', fontFamily: 'Montserrat'}}>{app}</td>
     </tr>
   )
