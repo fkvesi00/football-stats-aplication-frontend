9116e35c041c6fa51c909308e50cd80af0069021
fkvesi00 <fkvesi00@fesb.hr>
2024-02-07 19:52:34 +0100

    lets see this

--- src/components/StandingsTable.jsx
+++ src/components/StandingsTable.jsx
@@ -1,38 +1,31 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 import Table from './Table';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrophy } from '@fortawesome/free-solid-svg-icons';
+import {  faTrophy   } from '@fortawesome/free-solid-svg-icons';
 
 const StandingsTable = ({ tablica }) => {
   const rows = tablica.map((clubStats, i) => (
-    <motion.tr
+    <Table
       key={i}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
-      <Table
-        rank={i + 1}
-        name={clubStats.name}
-        played={clubStats.won + clubStats.lost + clubStats.draw}
-        w={clubStats.won}
-        d={clubStats.draw}
-        l={clubStats.lost}
-        gf={clubStats.gf}
-        ga={clubStats.ga}
-        points={clubStats.points}
-        id={clubStats.id}
-      />
-    </motion.tr>
+      rank={i + 1}
+      name={clubStats.name}
+      played={clubStats.won + clubStats.lost + clubStats.draw}
+      w={clubStats.won}
+      d={clubStats.draw}
+      l={clubStats.lost}
+      gf={clubStats.gf}
+      ga={clubStats.ga}
+      points={clubStats.points}
+      id={clubStats.id}
+    />
   ));
 
   return (
     <div className="overflow-x-auto my-10 mr-1 ml-1 mt-5 mb-5 mx-auto max-w-[2/3]">
-      <div className="header text-center mb-2 mt-2">
-        {'Ljestvica'} <FontAwesomeIcon icon={faTrophy} style={{ color: 'gold' }}/>
-      </div>
-      <table className="table-auto mx-auto" data-theme="night">
+    <div className="header text-center mb-2 mt-2">
+     {'Ljestvica'} <FontAwesomeIcon icon={faTrophy} style={{ color: 'gold' }}/>
+</div>
+    <table className="table-auto mx-auto" data-theme="night">
         <thead>
           <tr>
             <th className="p-1 text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">Rank</th>
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,31 +1,39 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React from 'react';
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 
-function Table({rank,name,played,w,d,l,gf,ga,points,id}) {
-  const logo = `/images/${id}.jpg`
+function Table({ rank, name, played, w, d, l, gf, ga, points, id }) {
+  const logo = `/images/${id}.jpg`;
+
   return (
-    <tr data-theme='fantasy' className='p-2 m-2'>
-    <td className='bg-neutral text-white w-3 h-3 rounded-full text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>
-  <div className="flex items-center justify-center p-2">
-    {rank}
-  </div>
-</td>
-<td className='p-2 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>
+    <motion.tr
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      data-theme='fantasy'
+      className='p-2 m-2'
+    >
+      <td className='bg-neutral text-white w-3 h-3 rounded-full text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>
+        <div className="flex items-center justify-center p-2">
+          {rank}
+        </div>
+      </td>
+      <td className='p-2 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>
         <Link to={`klub/${id}`} className="flex items-center">
           <img src={logo} alt={`Logo for ${name}`} width="30" height="30" className="mr-2 rounded-full" />
           <span >{name}</span>
         </Link>
       </td>
-    <td className='text-center p-1 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>{played}</td>
-    <td className='text-center p-1 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>{w}</td>
-    <td className='text-center p-1 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>{d}</td>
-    <td className='text-center p-1 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>{l}</td>
-    <td className='text-center p-1 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl hidden sm:table-cell'>{gf}</td>
-    <td className='text-center p-1 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl hidden sm:table-cell'>{ga}</td>
-    <td className='text-center p-2 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>{gf - ga}</td>
-    <td className='text-center p-2 text-center text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl' style={{ fontWeight: 'bold' }} data-theme="garden">{points}</td>
-  </tr>
+      <td className='text-center p-1 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>{played}</td>
+      <td className='text-center p-1 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>{w}</td>
+      <td className='text-center p-1 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>{d}</td>
+      <td className='text-center p-1 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>{l}</td>
+      <td className='text-center p-1 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl hidden sm:table-cell'>{gf}</td>
+      <td className='text-center p-1 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl hidden sm:table-cell'>{ga}</td>
+      <td className='text-center p-2 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>{gf - ga}</td>
+      <td className='text-center p-2 text-center text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl' style={{ fontWeight: 'bold' }} data-theme="garden">{points}</td>
+    </motion.tr>
   )
 }
 
-export default Table
+export default Table;
