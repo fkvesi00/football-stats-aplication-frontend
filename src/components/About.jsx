3038880d0753a21a3854dd8b5330cf14aca00087
fkvesi00 <fkvesi00@fesb.hr>
2024-02-07 13:39:08 +0100

    lets see

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,18 +4,30 @@ import './about.css';
 
 function About() {
   const text = `Web aplikacija koja pruža informacije i statistiku o malonogometnoj ligi koja se igra na području Metkovića pod nazivom UMA-Metković`;
-
+  const logo = '/images/0.jpg'
   return (
     <div>
       <div className="about">
         <h1 style={{ textAlign: 'center', margin: '10px', padding:'5px' }}>UMA Metković</h1>
-        <div style={{ textAlign: 'center', margin: '0 auto', maxWidth: '100%', padding:'10px' }}>
-        <img
-            src="/images/0.jpg"
-            alt="About"
-            style={{ maxWidth: '100%', maxHeight: '400px', height: 'auto', margin:'auto', padding:'10px' }}
-          />
-        </div>
+        <div className="mb-4 md:mb-0 md:mr-8"
+  style={{
+    borderRadius: '10px',
+    padding: '20px',
+    backgroundColor: '#ffffff',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    transition: 'box-shadow 0.3s ease',
+    overflow: 'hidden',
+  }}
+>
+  <div style={{ borderRadius: '10px', overflow: 'hidden' }}>
+    <img
+      src={logo}
+      alt="Club Logo"
+      className="w-40 h-auto md:w-40"
+      style={{ borderRadius: '10px 10px 0 0', objectFit: 'cover' }}
+    />
+  </div>
+</div>
         <p style={{ fontFamily: '"Times New Roman", Times, serif;', margin: '10px', padding:'5px' }}>{text}</p>
         
         <p style={{ fontFamily: '"Times New Roman", Times, serif;', margin: '10px', padding:'5px' }}>Version: 1.0.0</p>
