aece9dccce16eabc2e0e796e10e34329fadc2a0f
fkvesi00 <fkvesi00@fesb.hr>
2024-02-07 19:21:12 +0100

    implementing all games of club

--- src/components/UtakmiceKluba.jsx
+++ src/components/UtakmiceKluba.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Utakmica from './Utakmica';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFutbol,faUser } from '@fortawesome/free-solid-svg-icons';
+import { faFutbol } from '@fortawesome/free-solid-svg-icons';
 
 function UtakmiceKluba({utakmice}) {
     const nizUtakmica = utakmice.map((utakmica,i)=>{
