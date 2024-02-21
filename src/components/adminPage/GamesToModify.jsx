83dee4cc60f15b26fe21a351cd6edb59e48c05ed
fkvesi00 <fkvesi00@fesb.hr>
2024-02-21 11:22:06 +0100

    trying to make addGoalapp compoentn more responsive

--- src/components/adminPage/GamesToModify.jsx
+++ src/components/adminPage/GamesToModify.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 
-function GamesToModify({time,date,home,away, handleInputHomeScore, handleInputAwayScore}) {
+function GamesToModify({ time, date, home, away, handleInputHomeScore, handleInputAwayScore }) {
 
   return (
-       <tr>
-            <td>{date}</td>
-            <td style={{textAlign:'center'}}>{time}</td>
-            <td style={{textAlign:'end'}}>{home}</td>
-            <td style={{backgroundColor:'#130F2A', fontSize:'20px',fontWeight: 'bold',color:'white',textAlign:'center'}}> 
-            <input type="number" min='0' style={{ textAlign: 'center' }} onChange={handleInputHomeScore}/>
-            VS 
-            <input type='number' min='0' style={{ textAlign: 'center' }} onChange={handleInputAwayScore}/>
-            </td>
-            <td >{away}</td>
-      </tr>
+    <tr>
+      <td>{date}</td>
+      <td style={{ textAlign: 'center' }}>{time}</td>
+      <td style={{ textAlign: 'end' }}>{home}</td>
+      <td style={{ backgroundColor: '#130F2A', fontSize: '20px', fontWeight: 'bold', color: 'white', textAlign: 'center' }}>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <label htmlFor="homeScore">Home Score:</label>
+          <input id="homeScore" type="number" min='0' style={{ textAlign: 'center', marginBottom: '5px' }} onChange={handleInputHomeScore} />
+          <label htmlFor="awayScore">Away Score:</label>
+          <input id="awayScore" type='number' min='0' style={{ textAlign: 'center' }} onChange={handleInputAwayScore} />
+        </div>
+      </td>
+      <td>{away}</td>
+    </tr>
   )
 }
 
