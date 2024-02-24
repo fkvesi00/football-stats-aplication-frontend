3c0672e9a0dbf682d12691d9cd1ec73577b3dce7
fkvesi00 <fkvesi00@fesb.hr>
2024-02-24 16:16:40 +0100

    making it more responsive

--- src/components/adminPage/GamesToModify.jsx
+++ src/components/adminPage/GamesToModify.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 function GamesToModify({ time, date, home, away, handleInputHomeScore, handleInputAwayScore }) {
 
@@ -6,18 +6,18 @@ function GamesToModify({ time, date, home, away, handleInputHomeScore, handleInp
     <tr>
       <td>{date}</td>
       <td style={{ textAlign: 'center' }}>{time}</td>
-      <td style={{ textAlign: 'end' }}>{home}</td>
-      <td style={{ backgroundColor: '#130F2A', fontSize: '20px', fontWeight: 'bold', color: 'white', textAlign: 'center' }}>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <label htmlFor="homeScore">H Score:</label>
-          <input id="homeScore" type="number" min='0' style={{ textAlign: 'center', marginBottom: '5px' }} onChange={handleInputHomeScore} />
-          <label htmlFor="awayScore">A Score:</label>
-          <input id="awayScore" type='number' min='0' style={{ textAlign: 'center' }} onChange={handleInputAwayScore} />
+      <td>{home}</td>
+      <td style={{ textAlign: 'center', backgroundColor: '#130F2A', fontSize: '20px', fontWeight: 'bold', color: 'white' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <label htmlFor="homeScore" style={{ marginBottom: '5px' }}>H Score:</label>
+          <input id="homeScore" type="number" min='0' style={{ textAlign: 'center', width: '50px' }} onChange={handleInputHomeScore} />
+          <label htmlFor="awayScore" style={{ marginTop: '5px' }}>A Score:</label>
+          <input id="awayScore" type='number' min='0' style={{ textAlign: 'center', width: '50px' }} onChange={handleInputAwayScore} />
         </div>
       </td>
       <td>{away}</td>
     </tr>
-  )
+  );
 }
 
-export default GamesToModify
+export default GamesToModify;
