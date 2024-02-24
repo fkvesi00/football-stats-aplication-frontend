f4ad1a97fcdd4d39a5cb522fc51edf540e54803b
fkvesi00 <fkvesi00@fesb.hr>
2024-02-24 16:12:50 +0100

    minor changes

--- src/components/adminPage/GamesToModify.jsx
+++ src/components/adminPage/GamesToModify.jsx
@@ -9,9 +9,9 @@ function GamesToModify({ time, date, home, away, handleInputHomeScore, handleInp
       <td style={{ textAlign: 'end' }}>{home}</td>
       <td style={{ backgroundColor: '#130F2A', fontSize: '20px', fontWeight: 'bold', color: 'white', textAlign: 'center' }}>
         <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <label htmlFor="homeScore">Home Score:</label>
+          <label htmlFor="homeScore">H Score:</label>
           <input id="homeScore" type="number" min='0' style={{ textAlign: 'center', marginBottom: '5px' }} onChange={handleInputHomeScore} />
-          <label htmlFor="awayScore">Away Score:</label>
+          <label htmlFor="awayScore">A Score:</label>
           <input id="awayScore" type='number' min='0' style={{ textAlign: 'center' }} onChange={handleInputAwayScore} />
         </div>
       </td>
