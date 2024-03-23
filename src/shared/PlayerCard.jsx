2ef5dc9bb85f71f5c30929d11a2b72e2eed18510
fkvesi00 <fkvesi00@fesb.hr>
2024-03-23 12:21:04 +0100

    adding bigger frame

--- src/shared/PlayerCard.jsx
+++ src/shared/PlayerCard.jsx
@@ -7,7 +7,7 @@ function PlayerCard({ime,godine,nacionalnost,slika,id}) {
     <Link to={`/igrac/${id}`}>
       
     <div className="card w-64 bg-base-150 shadow-xl p-4 m-2 " >
-    <figure style={{height:"100px"}}><img src={imgPath} alt="" /></figure>
+    <figure style={{height:"150px"}}><img src={imgPath} alt="" /></figure>
     <div className="card-body">
       <h2 className="card-title">{ime}</h2>
       <p>Godina: {godine}</p>
