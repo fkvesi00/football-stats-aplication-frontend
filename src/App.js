2892adec18fbe044e1650a3596be8b3d2fb4e587
fkvesi00 <fkvesi00@fesb.hr>
2024-07-02 22:34:28 +0200

    cleaning app component 1/2

--- src/App.js
+++ src/App.js
@@ -24,9 +24,6 @@ import { ScheduleProvider } from "./context/scheduleContext/ScheduleContext";
 import { MatchProvider } from "./context/matchContext/MatchContext";
 
 function App() {
-  const [klubovi, setKlubovi] = useState([]);
-   const [raspored, setRaspored] = useState([]);
-   const stats = []
    const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -39,181 +36,7 @@ function App() {
     return () => clearTimeout(delay);
   }, []);
 
-  useEffect(()=>{
-    const fetchData = async () => {
-     
-      const raspored = await fetch("https://www.umadomena.com/matches/allMatches",{
-        method:'POST',
-        headers:{'Content-Type':'application/json'},
-        body:JSON.stringify({
-          seasonID:1
-        })
-      })
-      
-      const team = await fetch("https://www.umadomena.com/clubs/season",{
-        method:'POST',
-        headers:{'Content-Type':'application/json'},
-        body:JSON.stringify({
-           seasonID:1
-        })
-      })
 
-      const json1 = await team.json();
-      const json2 = await raspored.json();
-     
-      
-      setRaspored(json2)
-      setKlubovi(json1)
-      
-    }
-   fetchData()
-
-  
-},[])
-
-
-const matchFormat = utakmica => {
-  const matches = [];
-
-  utakmica.forEach((utakmica2, j) => {
-    for (let i = j + 1; i < utakmica.length; i++) {
-      if (utakmica2.matchid === utakmica[i].matchid) {
-        const homeTeam = utakmica2.home ? utakmica2 : utakmica[i];
-        const awayTeam = utakmica2.home ? utakmica[i] : utakmica2;
-
-        const match = {
-          match_id: utakmica2.matchid,
-          date: utakmica2.date,
-          time: utakmica2.time,
-          h_team: homeTeam.teamname,
-          h_id: homeTeam.teamid,
-          score: utakmica2.score,
-          a_team: awayTeam.teamname,
-          a_id: awayTeam.teamid
-        };
-
-        matches.push(match);
-      }
-    }
-  });
-
-  return matches;
-};
-  
-  const matches = matchFormat(raspored)
-
-
-
-//u sljedece dvije linije nalazimo utakmice koje su odigrane i koje ce se odigrati
-const matchesplayed = matches.filter(utakmica => utakmica.score !== null)
-const compareByDate = (a, b) => {
-  const dateA = new Date(a.date);
-  const dateB = new Date(b.date);
-
-  return dateB - dateA;
-};
-
-// Sort the array by date
-const mathesPlayedSorted = matchesplayed.sort(compareByDate);
-
-
-
-const matchesToPlay = matches.filter(utakmica => utakmica.score === null)
-
-//ovdje cemo stvorit legendarni objekt sa teamid i teamname i svim utakmicama tog tima
-const teamMatches = (teams, allMatches) => {
-  const arrayOfArrays = [];
-
-  teams.forEach((team) => {
-    const matchesOfTeam = allMatches.reduce((accumulator, match) => {
-      if (team.teamid === match.a_id || team.teamid === match.h_id) {
-        accumulator.push(match);
-      }
-      return accumulator;
-    }, []);
-
-    const object = {
-      id: team.teamid,
-      name: team.teamname,
-      matches: matchesOfTeam,
-    };
-
-    arrayOfArrays.push(object);
-  });
-
-  return arrayOfArrays;
-};
-
-  const allGamesByClub = teamMatches(klubovi, mathesPlayedSorted)
-  
-  
-  allGamesByClub.forEach(club =>{
-    const clubStats = {
-      id:0,
-      won: 0,
-      draw: 0,
-      lost: 0,
-      points: 0,
-      gf: 0,
-      ga: 0,
-      pm: 0,
-      name: '',
-      rank:''
-    }
-    
-  clubStats.id = club.id
-  clubStats.name = club.name
-    for (const utakmica of club.matches){
-     
-    if(utakmica.h_id === club.id){
-      clubStats.gf+=parseInt(utakmica.score[0])
-      clubStats.ga+=parseInt(utakmica.score[2])
-      clubStats.pm=clubStats.gf-clubStats.ga
-      if(utakmica.score[0] > utakmica.score[2]){
-        clubStats.points+=3
-        clubStats.won++;
-      }
-      else if(utakmica.score[0] < utakmica.score[2]){
-        clubStats.lost++
-      }
-      else {
-        clubStats.draw++;
-        clubStats.points++;
-      }
-    }else{
-      clubStats.gf+=parseInt(utakmica.score[2])
-      clubStats.ga+=parseInt(utakmica.score[0])
-      clubStats.pm= clubStats.gf - clubStats.ga
-      if(utakmica.score[2] > utakmica.score[0]){
-        clubStats.won++;
-        clubStats.points+=3;
-      }else if(utakmica.score[2] < utakmica.score[0]){
-        clubStats.lost++
-      }else{
-        clubStats.draw++;
-        clubStats.points++;
-      }
-    }
-  }
-    if(clubStats.id === 10){
-      clubStats.points -=6
-    }
-    stats.push(clubStats)
-  })
-
-  const sortedStats = stats.sort((a, b) => {
-    if (b.points !== a.points) {
-      // Sort by points in descending order
-      return b.points - a.points;
-    } else {
-      const clubB = b.gf - b.ga
-      const clubA = a.gf - a.ga
-      // If points are the same, sort by gf in ascending order
-      return clubA - clubB;
-    }
-  });
-  sortedStats.map((club,i) => club.rank=i)
-  
 
   return (
     <AuthProvider>
@@ -237,7 +60,7 @@ const teamMatches = (teams, allMatches) => {
             path='/'
             element={
               <>
-                <StandingsTable tablica={sortedStats} />
+                <StandingsTable />
                 <Utakmice />
                 <Raspored />
               </>
--- src/components/table/StandingsTable.jsx
+++ src/components/table/StandingsTable.jsx
@@ -1,10 +1,128 @@
-import React from 'react';
+import {useEffect, useContext} from 'react';
 import Table from './Table';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faTrophy   } from '@fortawesome/free-solid-svg-icons';
+import MatchContext from '../../context/matchContext/MatchContext';
+import ClubContext from '../../context/clubContext/ClubContext';
 
-const StandingsTable = ({ tablica }) => {
-  const rows = tablica.map((clubStats, i) => (
+const StandingsTable = () => {
+  const {allMatches, loadAllMatches} = useContext(MatchContext)
+  const {clubs, loadClubs} = useContext(ClubContext)
+  const stats = []
+  useEffect(()=> {
+    loadAllMatches()
+    loadClubs()
+  },[])
+
+  const matchesplayed = allMatches.filter(utakmica => utakmica.score !== null)
+
+  const compareByDate = (a, b) => {
+  const dateA = new Date(a.date);
+  const dateB = new Date(b.date);
+
+  return dateB - dateA;
+};
+
+// Sort the array by date
+const mathesPlayedSorted = matchesplayed.sort(compareByDate);
+
+
+//ovdje cemo stvorit legendarni objekt sa teamid i teamname i svim utakmicama tog tima
+const teamMatches = (teams, allMatches) => {
+  const arrayOfArrays = [];
+
+  teams.forEach((team) => {
+    const matchesOfTeam = allMatches.reduce((accumulator, match) => {
+      if (team.teamid === match.a_id || team.teamid === match.h_id) {
+        accumulator.push(match);
+      }
+      return accumulator;
+    }, []);
+
+    const object = {
+      id: team.teamid,
+      name: team.teamname,
+      matches: matchesOfTeam,
+    };
+
+    arrayOfArrays.push(object);
+  });
+
+  return arrayOfArrays;
+};
+
+  const allGamesByClub = teamMatches(clubs, mathesPlayedSorted)
+  
+  
+  allGamesByClub.forEach(club =>{
+    const clubStats = {
+      id:0,
+      won: 0,
+      draw: 0,
+      lost: 0,
+      points: 0,
+      gf: 0,
+      ga: 0,
+      pm: 0,
+      name: '',
+      rank:''
+    }
+    
+  clubStats.id = club.id
+  clubStats.name = club.name
+    for (const utakmica of club.matches){
+     
+    if(utakmica.h_id === club.id){
+      clubStats.gf+=parseInt(utakmica.score[0])
+      clubStats.ga+=parseInt(utakmica.score[2])
+      clubStats.pm=clubStats.gf-clubStats.ga
+      if(utakmica.score[0] > utakmica.score[2]){
+        clubStats.points+=3
+        clubStats.won++;
+      }
+      else if(utakmica.score[0] < utakmica.score[2]){
+        clubStats.lost++
+      }
+      else {
+        clubStats.draw++;
+        clubStats.points++;
+      }
+    }else{
+      clubStats.gf+=parseInt(utakmica.score[2])
+      clubStats.ga+=parseInt(utakmica.score[0])
+      clubStats.pm= clubStats.gf - clubStats.ga
+      if(utakmica.score[2] > utakmica.score[0]){
+        clubStats.won++;
+        clubStats.points+=3;
+      }else if(utakmica.score[2] < utakmica.score[0]){
+        clubStats.lost++
+      }else{
+        clubStats.draw++;
+        clubStats.points++;
+      }
+    }
+  }
+    if(clubStats.id === 10){
+      clubStats.points -=6
+    }
+    stats.push(clubStats)
+  })
+
+  const sortedStats = stats.sort((a, b) => {
+    if (b.points !== a.points) {
+      // Sort by points in descending order
+      return b.points - a.points;
+    } else {
+      const clubB = b.gf - b.ga
+      const clubA = a.gf - a.ga
+      // If points are the same, sort by gf in ascending order
+      return clubA - clubB;
+    }
+  });
+  sortedStats.map((club,i) => club.rank=i)
+ 
+
+  const rows = sortedStats.map((clubStats, i) => (
     <Table
       key={i}
       rank={i + 1}
