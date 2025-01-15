afcde679528cd9407b0873ba0d69cb6c925fe7dd
fkvesi00 <fkvesi00@fesb.hr>
2025-01-15 22:08:22 +0100

    adding my photo

--- src/shared/PlayerCard2.jsx
+++ src/shared/PlayerCard2.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 function PlayerCard2({ime,godine,nacionalnost,slika,id}) {
-  const imgPath = id === 106 ? `/images/players/nikola/${id}.jpeg` : "images/dummy-profile.jpg" 
+  const imgPath = id === 106 ? `/images/players/nikola/${id}.jpg` : "images/dummy-profile.jpg" 
   
   return (
     <Link to={`/igrac/${id}`}>
