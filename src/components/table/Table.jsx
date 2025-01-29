9c8d84c94271a632b699c0fc77e413f5b9782f8c
fkvesi00 <fkvesi00@fesb.hr>
2025-01-29 21:47:04 +0100

    adding last photo of team and table adaptation becuasse of WAs

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -4,7 +4,14 @@ import { Link } from 'react-router-dom';
 
 function Table({ rank, name, played, w, d, l, gf, ga, points, id }) {
   const logo = `/images/${id}.jpg`;
-  
+// Adjust gf and ga based on id conditions
+let adjustedGF = gf;
+let adjustedGA = ga;
+
+if (id === 13) adjustedGA += 2;
+if (id === 7) adjustedGF += 2;
+if (id === 11) adjustedGA++;
+if (id === 5) adjustedGF++;
   return (
     <motion.tr
       initial={{ opacity: 0 }}
@@ -28,8 +35,8 @@ function Table({ rank, name, played, w, d, l, gf, ga, points, id }) {
       <td className='text-center px-1 py-2 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>{w}</td>
       <td className='text-center px-1 py-2 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>{d}</td>
       <td className='text-center px-1 py-2 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>{l}</td>
-      <td className='text-center px-1 py-2 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl '>({gf}:{ga})</td>
-      <td className='text-center px-1 py-2 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl hidden sm:table-cell'>{gf - ga}</td>
+      <td className='text-center px-1 py-2 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl '>({adjustedGF}:{adjustedGA})</td>
+      <td className='text-center px-1 py-2 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl hidden sm:table-cell'>{adjustedGF - adjustedGA}</td>
       <td className='text-center px-1 py-2 text-center text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl' style={{ fontWeight: 'bold' }} data-theme="garden">{points}</td>
     </motion.tr>
   )
